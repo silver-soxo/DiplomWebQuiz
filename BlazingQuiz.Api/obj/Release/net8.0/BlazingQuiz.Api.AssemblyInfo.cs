1f60ad717d1802171caa722c15851fb907fc644b
moonshine666 <rukorko@yandex.com>
2025-05-01 15:01:31 +0300

    Reapply "del txt file"
    This reverts commit b92db1706b9fb50316dfd9dcfac13a90d8b61a0a.

--- BlazingQuiz.Api/obj/Release/net8.0/BlazingQuiz.Api.AssemblyInfo.cs
+++ BlazingQuiz.Api/obj/Release/net8.0/BlazingQuiz.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazingQuiz.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6826e667fefece5f18a4dfebd898c0ed64ab5b00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66e6a31f49030f4239264394b04cb52dde95b084")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazingQuiz.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazingQuiz.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
